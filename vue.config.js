1cc204735e165f7bac55959678e54d5fb00804b8
Guga Dam <lesmonades@gmail.com>
2024-06-14 00:57:17 -0300

    eslint-disable

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
-// webpack.config.js
-/* eslint-disable no-undef */
+// /* eslint-disable no-undef */
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  }
+})
+
 const { VuetifyPlugin } = require('webpack-plugin-vuetify')
 
 module.exports = {
