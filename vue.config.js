dc47e700792495b45dba11e97faff2a5cd34840c
Guga Dam <lesmonades@gmail.com>
2024-06-14 01:01:46 -0300

    import instead of requires

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,25 @@
-/* eslint-disable no-undef */
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// Importa defineConfig diretamente do módulo @vue/cli-service
+import { defineConfig } from '@vue/cli-service'
+
+// Configuração inicial usando defineConfig
+export default defineConfig({
   pluginOptions: {
     vuetify: {
-      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+      // Aqui você pode configurar opções específicas do Vuetify, se necessário
+      // Exemplo: theme: { dark: true }
+      // Mais opções em: https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     }
   }
+
+  // Configuração do Vue CLI Service pode continuar aqui, se necessário
 })
 
-const { VuetifyPlugin } = require('webpack-plugin-vuetify')
+// Importa VuetifyPlugin diretamente do módulo webpack-plugin-vuetify
+import { VuetifyPlugin } from 'webpack-plugin-vuetify'
 
-module.exports = {
+// Adiciona o VuetifyPlugin aos plugins de webpack
+export const configureWebpack = {
   plugins: [
-    new VuetifyPlugin({ autoImport: true }) // Enabled by default
+    new VuetifyPlugin({ autoImport: true }) // autoImport habilitado por padrão
   ]
 }
