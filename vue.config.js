8e2dc6b8e121fd1459a0e6b148582855ff25de4e
Guga Dam <lesmonades@gmail.com>
2024-06-14 00:52:02 -0300

    updates vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
+// webpack.config.js
 /* eslint-disable no-undef */
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  pluginOptions: {
-    vuetify: {
-      https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-    }
-  }
-})
+const { VuetifyPlugin } = require('webpack-plugin-vuetify')
+
+module.exports = {
+  plugins: [
+    new VuetifyPlugin({ autoImport: true }) // Enabled by default
+  ]
+}
