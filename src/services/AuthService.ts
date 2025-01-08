0cbb35963474025113d4f16a2f6dc4819b003c28
Guga Dam <lesmonades@gmail.com>
2025-01-08 00:50:14 -0300

    removes all .ts from services

--- src/services/AuthService.ts
+++
@@ -1,9 +0,0 @@
-import axiosInterceptor from './axiosInterceptor'
-
-const API_URL = `${import.meta.env.VITE_AUTH_URL}/auth`
-
-export default {
-  userTokenInfo() {
-    return axiosInterceptor.get(`${API_URL}/userinfo`)
-  }
-}
